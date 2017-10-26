65575246965bc438d01a29098ee6f710cca5acc5
Petar Rimac <petar.rimac@gmail.com>
2017-10-26 19:44:10 +0200

    Napravljeni viewovi za kuharicu

---
+++ resources/views/index.blade.php
@@ -0,0 +1,28 @@
+@extends('master')
+
+@section('title', 'Recepti')
+
+@section('content')
+
+<div class="container">
+	<div class="col-md-10 col-md-offset-1">
+		<div class="panel panel-defaut">
+			<div class="panel-heading">Recepti<br>
+				<a href="{{ url ('recipes/add') }}"></a>
+				<i class="fa fa-btn fa-plus"></i>
+				Dodaj novi recept
+			</div>
+			<div class="panel-body">
+				<ul class="list-group">
+					@foreach ($recipes as $recipe)
+					<li class="list-group-item">
+						<a href="recipes/view/{{ $recipe->id }}">{{ $recipe->name }}</a>
+					</li>
+					@endforeach
+				</ul>
+			</div>
+		</div>
+	</div>
+</div>
+
+@endsection
